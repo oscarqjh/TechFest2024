6ece6557a9585645664c8b54ac6bab6070a22c68
oscarqjh <oscar.jh9@gmail.com>
2024-02-04 22:01:09 +0800

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./routes/ErrorPage";
+import LandingPage from "./routes/LandingPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/base.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/routes/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>Oops! Something went wrong.</h1>
+    </div>
+  );
+}
