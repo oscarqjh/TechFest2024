c29e7612796518832d57d5af739f8631749242c2
oscarqjh <oscar.jh9@gmail.com>
2024-02-04 22:29:45 +0800

    import axios

---
+++ frontend/src/api/common.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// export default axios.create({
+//   baseURL: //gpt-3-api-url,
+
+//   credentials: true,
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+// });
