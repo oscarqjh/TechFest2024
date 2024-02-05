9f8db674ebad0a5ef4348c611d7a4f6014a0df22
Oscarqjh <aerialguard@gmail.com>
2024-02-06 04:53:51 +0800

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,10 +29,6 @@ const router = createBrowserRouter([
     element: <TestApp />,
   },
   {
-    path: "/test",
-    element: <TestApp />,
-  },
-  {
     path: "/app",
     element: <App />,
   },
